e569ed2ce6383c638b63beefd9a1e233ad2d4d12
rfabisiak <fabisiakradoslaw@gmail.com>
2020-09-24 10:38:48 +0200

    Start here

---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home(props) {
+  
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <div>Duomly Next.js app starter</div>
+    </div>
+  );
+}
